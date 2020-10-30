70c171452b74fb8ddd8e95dd356e5d6162e5d24b
ihab abadi <ihab@utopios.net>
2020-10-30 11:09:11 +0100

    suite cours nodejs

--- cours-nodejs/app.js
+++ cours-nodejs/app.js
@@ -2,7 +2,18 @@
 //const http = require("http");
 //Après installation de express, on importe le module express dans notre fichier
 const express = require("express");
-
+const produits = [
+    {
+        id : 1,
+        title : "produit 1",
+        price : 10
+    },
+    {
+        id : 2,
+        title : "produit 2",
+        price : 10
+    },
+]
 
 //On crée notre application en utilisant express
 const app = express()
@@ -15,7 +26,28 @@ app.get('/', (req,res) => {
 
 //On intercepte une requete en get sur la route /produits
 app.get('/produits', (req,res) => {
-    res.end("voici les produits")
+    //res.end("voici les produits")
+    //Envoyer la réponse en json
+    res.json(produits)
+})
+
+//Une route pour chercher un produit en fonction de son id
+app.get('/produit/:id', (req,res) => {
+    //on peut récupérer dans la requete le paramètre id
+    const id = req.params.id
+    const produit = produits.find(p => p.id == id)
+    if(produit != undefined) {
+        res.json({produit : produit , error : false})
+    }
+    else {
+        res.json({message : "product not found", error : true})
+    }
+
+})
+
+
+app.post('/produits', (req,res) => {
+    res.json(produits)
 })
 
 
