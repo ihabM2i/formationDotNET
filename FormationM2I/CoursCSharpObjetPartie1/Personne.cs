18f9ecd8f80a9f8a26eafe8b31fb116483f353bc
ihab abadi <ihab@utopios.net>
2020-11-18 17:00:46 +0100

    cours objet partie 1

---
+++ FormationM2I/CoursCSharpObjetPartie1/Personne.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoursCSharpObjetPartie1
+{
+    class Personne
+    {
+        //Attributs
+        private string nom;
+        private string prenom;
+        private int age;
+        private Adresse adresse;
+
+        //Proprietés
+
+        public string Nom
+        {
+            get
+            {
+                return nom;
+            }
+            set
+            {
+                if (value.Length > 2)
+                    nom = value;
+                else
+                    Console.WriteLine("Erreur nom");
+            }
+        }
+
+        //Constructeurs
+        public Personne()
+        {
+            Console.WriteLine("Construction d'un objet");
+        }
+
+        public Personne(string nom, string prenom, int age)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.age = age;
+        }
+
+        //Méthodes
+        public void Afficher()
+        {
+            Console.WriteLine(nom + " " + prenom);
+        }
+
+        //public void SetNom(string n)
+        //{
+        //    if(n.Length > 2)
+        //    {
+        //        nom = n;
+        //    }
+        //    Console.WriteLine("Erreur saisi nom");
+        //}
+        //public string GetNom()
+        //{
+        //    return nom;
+        //}
+
+    }
+}
---
+++ FormationM2I/CoursCSharpObjetPartie1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CoursCSharpObjetPartie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne p1 = new Personne();
+            //p1.nom = "abadi";
+            //p1.prenom = "ihab";
+            //Adresse a = new Adresse();
+            //a.adresse = "tilleul";
+            //a.ville = "Tourcoing";
+            //p1.adresse = a;
+            //Console.WriteLine(p1.nom + " " + p1.prenom);
+            //p1.SetNom("abadi");
+            p1.Nom = "abadi";
+            p1.Afficher();
+            Personne p2 = new Personne();
+            //p2.nom = "tata";
+            //p2.prenom = "toto";
+            //Console.WriteLine(p2.nom + " " + p2.prenom);
+            p2.Afficher();
+            Personne p3 = new Personne("titi", "minet", 50);
+            p3.Afficher();
+        }
+    }
+}
