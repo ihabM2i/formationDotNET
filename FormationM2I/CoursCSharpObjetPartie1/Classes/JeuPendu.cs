82bf4ca08698808059bd6d962ffc8848e2be4c6c
ihab abadi <ihab@utopios.net>
2020-11-30 16:28:50 +0100

    cours mock tdd

---
+++ FormationM2I/CoursCSharpObjetPartie1/Classes/JeuPendu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoursCSharpObjetPartie1.Classes
+{
+    public class JeuPendu
+    {
+        private IGenerateur generateur;
+        private string motATrouve;
+        private int nbreEssai;
+        private string masque;
+        public JeuPendu(IGenerateur g)
+        {
+            generateur = g;
+            motATrouve = g.Generer();
+            nbreEssai = 10;
+        }
+
+        public int NbreEssai { get => nbreEssai; }
+        public string Masque { get => masque; }
+
+        public bool TestChar(char c)
+        {
+            throw new Exception("en cours de dev");
+        }
+
+        public bool TestWin()
+        {
+            throw new Exception("en cours de dev");
+        }
+
+        public void GenererMasque()
+        {
+            throw new Exception("en cours de dev");
+        }
+    }
+}
---
+++ FormationM2I/CoursTestUnitaire/JeuPenduTest.cs
@@ -0,0 +1,41 @@
+﻿using CoursCSharpObjetPartie1.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoursTestUnitaire
+{
+    [TestClass]
+    class JeuPenduTest
+    {
+        private IGenerateur g = Mock.Of<IGenerateur>();
+
+        [TestMethod]
+        public void GenererMasqueTest()
+        {
+            //Arrange
+            //FakeGenerateurMot g = new FakeGenerateurMot();
+            //IGenerateur g = Mock.Of<IGenerateur>();
+            Mock.Get(g).Setup(o => o.Generer()).Returns("bonjour");
+            JeuPendu jeu = new JeuPendu(g);
+            //Act
+            jeu.GenererMasque();
+            //Assert
+            Assert.AreEqual("*******", jeu.Masque);
+        }
+
+        [TestMethod]
+        public void TestCharTest()
+        {
+            //Arrange
+            Mock.Get(g).Setup(o => o.Generer()).Returns("coucou");
+            JeuPendu jeu = new JeuPendu(g);
+            //Act
+            bool result = jeu.TestChar('c');
+            //Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
