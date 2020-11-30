7a027a96e7cbc90adcf69e59ec919abb38fc5115
ihab abadi <ihab@utopios.net>
2020-11-30 14:12:38 +0100

    exemple TDDanne bissextile

---
+++ FormationM2I/CoursCSharpObjetPartie1/Classes/Tools.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoursCSharpObjetPartie1.Classes
+{
+    public class Tools
+    {
+        public bool IsBissextile(int annee)
+        {
+            //if(annee%4 == 0)
+            //{
+            //    if(annee%100 == 0 && annee%400 != 0)
+            //    {
+            //        return false;
+            //    }
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+
+            return (annee % 4 == 0 && !(annee % 100 == 0 && annee % 400 != 0));
+        }
+    }
+}
