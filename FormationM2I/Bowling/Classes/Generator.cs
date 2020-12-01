b366157219d2570fbe8ce484f61455fb71536bdb
ihab abadi <ihab@utopios.net>
2020-12-01 14:48:27 +0100

    correction bowling partie 1

---
+++ FormationM2I/Bowling/Classes/Generator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bowling.Classes
+{
+    public class Generator : IGenerator
+    {
+        public int GetRandomPins(int max)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
