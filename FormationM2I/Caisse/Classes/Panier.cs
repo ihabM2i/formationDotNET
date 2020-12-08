d89c1bf55f87b2a221f6bd1ece9db7080b14a917
ihab abadi <ihab@utopios.net>
2020-12-08 09:46:33 +0100

    correction caisse partie 1

---
+++ FormationM2I/Caisse/Classes/Panier.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Caisse.Classes
+{
+    public class Panier
+    {
+        private int id;
+        
+        private decimal total;
+
+        private List<Produit> produits;
+
+        public int Id { get => id; set => id = value; }
+        public decimal Total { get => total; }
+        public List<Produit> Produits { get => produits; set => produits = value; }
+
+        public Panier()
+        {
+            Produits = new List<Produit>();
+        }
+
+        public void AddProduit(Produit produit)
+        {
+            Produits.Add(produit);
+            UpdateTotal();
+        }
+
+        private void UpdateTotal()
+        {
+            decimal t = 0;
+            Produits.ForEach(p => { t += p.Prix; });
+            total = t;
+        }
+    }
+}
