86e81708b14b10ddac6c6b61a99164bb44d5dd9f
ihab abadi <ihab@utopios.net>
2020-12-09 10:38:44 +0100

    cours wpf listbox

---
+++ FormationM2I/coursWPF/Classes/Personne.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace coursWPF.Classes
+{
+    public class Personne
+    {
+        private string nom;
+        private string prenom;
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+
+        public override string ToString()
+        {
+            return Nom + " "+ prenom;
+        }
+    }
+}
---
+++ FormationM2I/coursWPF/ControleWPF.xaml.cs
@@ -0,0 +1,43 @@
+﻿using coursWPF.Classes;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace coursWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class ControleWPF : Window
+    {
+        private ObservableCollection<Personne> personnes;
+        public ControleWPF()
+        {
+            InitializeComponent();
+            personnes = new ObservableCollection<Personne>();
+            listePersonnes.ItemsSource = personnes;
+        }
+
+        private void AddClick(object sender, RoutedEventArgs e)
+        {
+            Personne p = new Personne()
+            {
+                Nom = textNom.Text,
+                Prenom = textPrenom.Text
+            };
+            personnes.Add(p);
+        }
+    }
+}
