b66e73cb372891f22567e57282be31e6fd309a91
ihab abadi <ihab@utopios.net>
2020-12-11 14:34:02 +0100

    correction ex1 mvvm caisse wpf

--- FormationM2I/CaisseWPF/UserControls/VenteControl.xaml.cs
+++ FormationM2I/CaisseWPF/UserControls/VenteControl.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Caisse.Classes;
+using CaisseWPF.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -20,62 +21,63 @@ namespace CaisseWPF.UserControls
     /// </summary>
     public partial class VenteControl : UserControl
     {
-        Panier panier;
-        ObservableCollection<Produit> produitsPanier;
+        //Panier panier;
+        //ObservableCollection<Produit> produitsPanier;
         public VenteControl()
         {
             InitializeComponent();
-            panier = new Panier();
-            produitsPanier = new ObservableCollection<Produit>(panier.Produits);
-            listViewProduits.ItemsSource = produitsPanier;
+            DataContext = new VenteControlViewModel();
+            //panier = new Panier();
+            //produitsPanier = new ObservableCollection<Produit>(panier.Produits);
+            //listViewProduits.ItemsSource = produitsPanier;
         }
 
-        private void Add_Click(object sender, RoutedEventArgs e)
-        {
-            int id;
-            if (Int32.TryParse(codeBox.Text, out id))
-            {
-                Produit p = DataBase.Instance.GetProduitById(id);
-                if (p != null)
-                {
-                    panier.AddProduit(p);
-                    produitsPanier.Add(p);
-                    DataBase.Instance.DescStock(p.Id);
-                    totalBlock.Content = panier.Total + " euros";
-                    codeBox.Text = "";
-                }
-                else
-                {
-                    MessageBox.Show("Aucun produit avec cet id");
-                }
-            }
-            else
-            {
-                MessageBox.Show("Erreur de code");
-            }
-        }
+        //private void Add_Click(object sender, RoutedEventArgs e)
+        //{
+        //    int id;
+        //    if (Int32.TryParse(codeBox.Text, out id))
+        //    {
+        //        Produit p = DataBase.Instance.GetProduitById(id);
+        //        if (p != null)
+        //        {
+        //            panier.AddProduit(p);
+        //            produitsPanier.Add(p);
+        //            DataBase.Instance.DescStock(p.Id);
+        //            totalBlock.Content = panier.Total + " euros";
+        //            codeBox.Text = "";
+        //        }
+        //        else
+        //        {
+        //            MessageBox.Show("Aucun produit avec cet id");
+        //        }
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show("Erreur de code");
+        //    }
+        //}
 
-        private void Confirm_Click(object sender, RoutedEventArgs e)
-        {
-            if(DataBase.Instance.SavePanier(panier))
-            {
-                MessageBox.Show("Panier validé");
-                panier = new Panier();
-                produitsPanier = new ObservableCollection<Produit>(panier.Produits);
-                listViewProduits.ItemsSource = produitsPanier;
-                totalBlock.Content = "0 euros";
-            }
-            else
-            {
-                MessageBox.Show("Erreur serveur");
-            }
-        }
+        //private void Confirm_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if(DataBase.Instance.SavePanier(panier))
+        //    {
+        //        MessageBox.Show("Panier validé");
+        //        panier = new Panier();
+        //        produitsPanier = new ObservableCollection<Produit>(panier.Produits);
+        //        listViewProduits.ItemsSource = produitsPanier;
+        //        totalBlock.Content = "0 euros";
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show("Erreur serveur");
+        //    }
+        //}
 
-        private void Update_Click(object sender, RoutedEventArgs e)
-        {
-            DataBase.Instance.UpdateDataBase();
-            MessageBox.Show("Mise à jour effectuée");
-        }
+        //private void Update_Click(object sender, RoutedEventArgs e)
+        //{
+        //    DataBase.Instance.UpdateDataBase();
+        //    MessageBox.Show("Mise à jour effectuée");
+        //}
 
 
     }
---
+++ FormationM2I/CaisseWPF/ViewModels/VenteControlViewModel.cs
@@ -0,0 +1,79 @@
+﻿using Caisse.Classes;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CaisseWPF.ViewModels
+{
+    public class VenteControlViewModel : ViewModelBase
+    {
+        private Panier panier;
+        private ObservableCollection<Produit> produits;
+        private int code;
+
+        public Panier Panier { get => panier; set => panier = value; }
+        public ObservableCollection<Produit> Produits { get => produits; set => produits = value; }
+        public int Code { get => code; set => code = value; }
+        public decimal Total { get => Panier.Total; }
+
+        public ICommand AddCommand { get; set; }
+
+        public ICommand ConfirmCommand { get; set; }
+
+        public ICommand UpdateCommand { get; set; }
+
+        public VenteControlViewModel()
+        {
+            Panier = new Panier();
+            Produits = new ObservableCollection<Produit>(Panier.Produits);
+            AddCommand = new RelayCommand(ActionAddCommand);
+            ConfirmCommand = new RelayCommand(ActionConfirmCommand);
+            UpdateCommand = new RelayCommand(ActionUpdateCommand);
+        }
+
+
+        private void ActionAddCommand()
+        {
+            Produit p = DataBase.Instance.GetProduitById(Code);
+            if (p != null)
+            {
+                Panier.AddProduit(p);
+                Produits.Add(p);
+                DataBase.Instance.DescStock(p.Id);
+                RaisePropertyChanged("Total");
+            }
+            else
+            {
+                MessageBox.Show("Aucun produit avec cet id");
+            }
+        }
+
+        private void ActionConfirmCommand()
+        {
+            if (DataBase.Instance.SavePanier(Panier))
+            {
+                MessageBox.Show("Panier validé");
+                Panier = new Panier();
+                Produits = new ObservableCollection<Produit>(Panier.Produits);
+                RaisePropertyChanged("Produits");
+                RaisePropertyChanged("Total");
+                
+            }
+            else
+            {
+                MessageBox.Show("Erreur serveur");
+            }
+        }
+
+        private void ActionUpdateCommand()
+        {
+            DataBase.Instance.UpdateDataBase();
+            MessageBox.Show("Mise à jour effectuée");
+        }
+    }
+}
