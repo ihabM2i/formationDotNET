88829f0138a9746d0ed1863c14517aabd7dabaf7
ihab abadi <ihab@utopios.net>
2020-12-15 12:26:17 +0100

    cours entityframework suite

--- FormationM2I/CoursEntityFrameWorkCore/ViewModels/MainViewModel.cs
+++ FormationM2I/CoursEntityFrameWorkCore/ViewModels/MainViewModel.cs
@@ -4,6 +4,7 @@ using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Input;
@@ -21,10 +22,13 @@ namespace CoursEntityFrameWorkCore.ViewModels
         public string LastName { get => person.LastName; set { person.LastName = value; RaisePropertyChanged(); } }
         public string Phone { get => person.Phone; set { person.Phone = value; RaisePropertyChanged(); } }
         
+        public string Search { get; set; }
+
         public ObservableCollection<Person> Persons { get; set; }
         public ICommand ConfirmCommand { get; set; }
         public ICommand SelectCommand { get; set; }
         public ICommand DeleteCommand { get; set; }
+        public ICommand SearchCommand { get; set; }
         public MainViewModel()
         {
             person = new Person();
@@ -33,6 +37,7 @@ namespace CoursEntityFrameWorkCore.ViewModels
             ConfirmCommand = new RelayCommand(ActionConfirm);
             SelectCommand = new RelayCommand(ActionSelect);
             DeleteCommand = new RelayCommand(ActionDelete);
+            SearchCommand = new RelayCommand(ActionSearch);
             SelectedPerson = null;
         }
 
@@ -70,6 +75,12 @@ namespace CoursEntityFrameWorkCore.ViewModels
                 data.SaveChanges();
                 Persons.Remove(SelectedPerson);
             }
+        }
+
+        private void ActionSearch()
+        {
+            Persons = new ObservableCollection<Person>(data.Persons.Where(p => p.FirstName.Contains(Search) || p.LastName.Contains(Search)));
+            RaisePropertyChanged("Persons");
         }
     }
 }
