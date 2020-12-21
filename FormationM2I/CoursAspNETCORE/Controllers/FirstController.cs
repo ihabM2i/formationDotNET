2548590670f13e9afd8aa0f79db716805c21fadb
ihab abadi <ihab@utopios.net>
2020-12-21 12:12:16 +0100

    Cours ASP.NET CORE MVC, Route, Controller, renvoie des view en razor

---
+++ FormationM2I/CoursAspNETCORE/Controllers/FirstController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoursAspNETCORE.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult Index()
+        {
+            return new ContentResult() { Content= "<h1>Bonjour tout le monde</h1>", ContentType="text/html" };
+        }
+
+        public IActionResult Contact()
+        {
+            return new JsonResult(new { Nom="abadi", Prenom="Ihab"});
+        }
+
+        public IActionResult FirstView()
+        {
+            return View();
+        }
+
+        public IActionResult SecondView()
+        {
+            return View("FirstView");
+        }
+
+        public IActionResult ThirdView()
+        {
+            return View("~/Views/Home/Privacy.cshtml");
+        }
+    }
+}
