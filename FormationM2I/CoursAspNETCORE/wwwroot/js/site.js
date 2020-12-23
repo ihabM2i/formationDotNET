7c254af68387f35bb883990ce17248a419bdf186
ihab abadi <ihab@utopios.net>
2020-12-23 16:35:15 +0100

    correction avatar contact

--- FormationM2I/CoursAspNETCORE/wwwroot/js/site.js
+++ FormationM2I/CoursAspNETCORE/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+//$("#form").on('submit', function (e) {
+//    e.preventDefault();
+//    $.ajax({
+//        url
+//    })
+//})
