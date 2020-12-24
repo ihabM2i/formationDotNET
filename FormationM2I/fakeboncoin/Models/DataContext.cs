29f93c2e281f5cbd8c9f8163c30133a78b5c83d6
ihab abadi <ihab@utopios.net>
2020-12-24 12:33:28 +0100

    correction annonce partie 1

---
+++ FormationM2I/fakeboncoin/Models/DataContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fakeboncoin.Models
+{
+    public class DataContext : DbContext
+    {
+        private static DataContext _instance;
+        
+        public DbSet<Annonce> Annonces { get; set; }
+        public DataContext()
+        {
+
+        }
+
+        public static DataContext Instance
+        {
+            get
+            {
+                if (_instance != null)
+                    _instance = new DataContext();
+                return _instance;
+            }
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(LocalDb)\coursM2i;Initial Catalog=entity;Integrated Security=True");
+        }
+    }
+}
